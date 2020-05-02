14ad557a9f04bd29ef290352df2c34d5ef879bc0
Nick Springer <nick_springer@trimble.com>
2020-05-02 10:59:48 -0400

    Changed swatch style and added click event

--- src/components/InputColor.jsx
+++ src/components/InputColor.jsx
@@ -57,6 +57,12 @@ export default class InputColor extends React.Component {
     this.setState({ pickerOpened: !this.state.pickerOpened })
   }
 
+  openPicker = () => {
+    if(!this.state.pickerOpened){
+      this.setState({ pickerOpened: true })
+    }
+  }
+
   get color() {
     // Catch invalid color.
     try {
@@ -115,7 +121,7 @@ export default class InputColor extends React.Component {
 
     return <div className="maputnik-color-wrapper">
       {this.state.pickerOpened && picker}
-      <div className="maputnik-color-swatch" style={swatchStyle}></div>
+      <div className="maputnik-color-swatch" style={swatchStyle} onClick={this.openPicker}></div>
       <input
         aria-label={this.props['aria-label']}
         spellCheck="false"
